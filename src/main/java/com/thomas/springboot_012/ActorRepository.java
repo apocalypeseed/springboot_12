268ab6d6713c64117be2e647c7ccca58e552b07d
apocalypeseed <tswlew@yahoo.com>
2018-07-17 11:34:31 -0400

    Completed version

---
+++ src/main/java/com/thomas/springboot_012/ActorRepository.java
@@ -0,0 +1,8 @@
+package com.thomas.springboot_012;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long>
+{
+
+}
---
+++ src/main/java/com/thomas/springboot_012/Springboot012Application.java
@@ -0,0 +1,12 @@
+package com.thomas.springboot_012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot012Application.class, args);
+	}
+}
